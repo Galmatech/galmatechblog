f058465abeab46e12577d7ca90d9f3c75a1b7d70
Galmatech <app@galmatech.com>
2023-04-02 10:26:42 +0000

    2 - Mettre en place le système de tests

---
+++ project/tests/Functional/BasicTest.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BasicTest extends WebTestCase
+{
+    public function testUnitEnvironnementIsOk(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+    }
+}
